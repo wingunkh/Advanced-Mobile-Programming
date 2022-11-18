dce48ef1b352f18e3a2ca1dfcde98f0d589db73f
wingunkh <99gusrms@naver.com>
2022-11-18 12:39:05 +0900

    파이어베이스 프로젝트 생성 및 로그인 기능을 통한 연결

--- app/src/main/java/com/example/go/Sign/LoginActivity.kt
+++ app/src/main/java/com/example/go/Sign/LoginActivity.kt
@@ -2,9 +2,13 @@ package com.example.go.Sign
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.go.Post.PostListActivity
 import com.example.go.databinding.ActivityLoginBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +22,23 @@ class LoginActivity : AppCompatActivity() {
         }
 
         binding.buttonSignIn.setOnClickListener{
-            val intent= Intent(this, PostListActivity::class.java)
-            startActivity(intent)
+            val userEmail = binding.loginEmail.text.toString()
+            val password = binding.loginPassword.text.toString()
+            doLogin(userEmail, password) //로그인 시도
         }
+    }
+
+    private fun doLogin(userEmail: String, password: String) {
+        Firebase.auth.signInWithEmailAndPassword(userEmail, password)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) { //로그인 성공 시
+                    startActivity(
+                        Intent(this, PostListActivity::class.java)) //메인 액티비티 시작
+                    finish()
+                } else { //로그인 실패 시
+                    Log.w("LoginActivity", "signInWithEmail", it.exception)
+                    Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
