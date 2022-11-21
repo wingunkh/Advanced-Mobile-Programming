2581863422822c40052edf4074330d7790c3003f
geonhee <garnett0122@hansung.ac.kr>
2022-11-21 16:43:18 +0900

    깃허브 연동

--- app/src/main/java/com/example/go/Utils/FBRef.kt
+++ app/src/main/java/com/example/go/Utils/FBRef.kt
@@ -1,15 +1,25 @@
-package com.example.go.Utils
+package com.example.dangdangee.Utils
 
+import android.content.ContentValues.TAG
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 
 object FBRef {
 
         private val database = Firebase.database
 
-        val postRef = database.getReference("post")
+        val boardRef = database.getReference("board")
 
         val commentRef = database.getReference("comment")
 
+        val mapRef = database.getReference("Marker")
+
         val userRef = database.getReference("User")
+
+
 }
