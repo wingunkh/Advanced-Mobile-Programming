ffd854ddcccee1a4f6a2f493de471fb4eab97e10
Yunjeong Cho <dbswjd1413@naver.com>
2022-11-21 16:22:29 +0900

    image_post_ui_almost_finish

---
+++ app/src/main/java/com/example/go/Utils/FBAuth.kt
@@ -0,0 +1,40 @@
+package com.example.dangdangee.Utils
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.userProfileChangeRequest
+import java.text.SimpleDateFormat
+import java.util.*
+
+object FBAuth {
+
+        val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+        fun getUid() : String{
+            return auth.currentUser?.uid.toString()
+        }
+
+        fun getEmail() : String{
+            return auth.currentUser?.email.toString()
+        }
+
+        fun getDisplayName() : String{
+          return auth.currentUser?.displayName.toString()
+        }
+
+        fun getTime() : String{
+            val currentDataTime = Calendar.getInstance().time
+            val dataFormat = SimpleDateFormat("yyyy.MM.dd HH:mm:ss",Locale.KOREA).format(currentDataTime)
+
+            return dataFormat
+        }
+
+        fun setDisplayName(name:String) {
+            auth.currentUser?.updateProfile(userProfileChangeRequest {
+                displayName = name
+            })
+        }
+
+        fun setPassword(pwd:String) {
+            auth.currentUser?.updatePassword(pwd)
+        }
+    }
