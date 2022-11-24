97b67cbff997afec49e26abb35defa973231127a
Yunjeong Cho <dbswjd1413@naver.com>
2022-11-25 07:45:57 +0900

    change menu location

---
+++ app/src/main/java/com/example/go/Search/SearchFragment.kt
@@ -0,0 +1,23 @@
+package com.example.go.Search
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.go.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = SearchFragment()
+    }
+}
